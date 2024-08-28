cd584376c1bbc2143252962893b2320272123e81
Anniymm <animaisuradze1999@gmail.com>
2024-08-28 22:18:27 +0400

    change root sizes

--- copy_tool_oop.py
+++ copy_tool_oop.py
@@ -11,7 +11,7 @@ class FileCopyApp:
     def __init__(self, root):
         self.root = root
         self.root.title("File Copier")
-        self.root.geometry("400x200")
+        self.root.geometry("500x300")
 
         self.label = Label(root, text="Choose files to copy:")
         self.label.pack(pady=10)
